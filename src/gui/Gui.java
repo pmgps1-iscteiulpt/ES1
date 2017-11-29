0b47e0fd37acf7f27d59e81b43c7589e06647da6
agasa <agasa@iscte.iul.pt>
2017-11-29 16:18:43 +0000

    Funcionalidades adicionadas:
    	- Leitura de ficheiro rules.cf e inserção dos dados na tabela
    	- Leitura da tabela após inserção de pesos manuais e criação de novo
    ficheiro com os respetivos pesos

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -4,6 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.LinkedList;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -15,21 +24,27 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import antiSpamFilter.Rule;
 
 public class Gui {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
+	private JTextField rulesTextField;
+	private JTextField spamTextField;
+	private JTextField hamTextField;
+	private JTextField fpTextField;
+	private JTextField fnTextField;
 	private JTable table;
+
+	private boolean editable; /* Serve para gerir a editabilidade da tabela */
+
+	private LinkedList<Rule> rulesList; /* Lista de regras */
 
 	/**
 	 * Launch the application.
 	 */
-	
 
 	/**
 	 * Create the application.
@@ -46,139 +61,221 @@ public class Gui {
 		frame.setBounds(100, 100, 900, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
-		panel.setLayout(new GridLayout(4, 1, 0, -5));
-		
-		JPanel panel_3 = new JPanel();
-		panel.add(panel_3);
-		
-		JLabel lblNewLabel_2 = new JLabel("Ham:");
-		panel_3.add(lblNewLabel_2);
-		
-		textField_2 = new JTextField();
-		panel_3.add(textField_2);
-		textField_2.setColumns(50);
-		
-		JButton btnNewButton_2 = new JButton("Upload");
-		panel_3.add(btnNewButton_2);
-		
-		JPanel panel_2 = new JPanel();
-		panel.add(panel_2);
-		
-		JLabel lblNewLabel_1 = new JLabel("Spam:");
-		panel_2.add(lblNewLabel_1);
-		
-		textField_1 = new JTextField();
-		panel_2.add(textField_1);
-		textField_1.setColumns(50);
-		
-		JButton btnNewButton_1 = new JButton("Upload");
-		panel_2.add(btnNewButton_1);
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JLabel lblNewLabel = new JLabel("Rules:");
-		panel_1.add(lblNewLabel);
-		
-		textField = new JTextField();
-		panel_1.add(textField);
-		textField.setColumns(50);
-		
-		JButton btnNewButton = new JButton("Upload");
-		panel_1.add(btnNewButton);
-		
-		JPanel panel_4 = new JPanel();
-		panel.add(panel_4);
-		
-		JLabel lblNewLabel_3 = new JLabel("Config:");
-		panel_4.add(lblNewLabel_3);
-		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Manual");
-		panel_4.add(chckbxNewCheckBox);
-		
-		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Autom\u00E1tica");
-		panel_4.add(chckbxNewCheckBox_1);
-		
-		JLabel lblFp = new JLabel("FP");
-		panel_4.add(lblFp);
-		
-		textField_3 = new JTextField();
-		panel_4.add(textField_3);
-		textField_3.setColumns(10);
-		
-		JLabel lblFn = new JLabel("FN");
-		panel_4.add(lblFn);
-		
-		textField_4 = new JTextField();
-		panel_4.add(textField_4);
-		textField_4.setColumns(10);
-		
-		JPanel panel_5 = new JPanel();
-		frame.getContentPane().add(panel_5, BorderLayout.SOUTH);
-		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JButton btnNewButton_3 = new JButton("Avaliar Configura\u00E7\u00E3o Manual");
-		btnNewButton_3.setHorizontalAlignment(SwingConstants.LEFT);
-		btnNewButton_3.setVerticalAlignment(SwingConstants.BOTTOM);
-		panel_5.add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("Gerar Configura\u00E7\u00E3o Autom\u00E1tica");
-		btnNewButton_4.setHorizontalAlignment(SwingConstants.RIGHT);
-		panel_5.add(btnNewButton_4);
-		
-		JButton btnObterGrfico = new JButton("Obter Gr\u00E1fico");
-		panel_5.add(btnObterGrfico);
-		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-				{null, null},
-			},
-			new String[] {
-				"Regras", "Pesos"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, String.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
+
+		/******* Painel de caminhos para os ficheiros ***************/
+		JPanel pathsPanel = new JPanel();
+		frame.getContentPane().add(pathsPanel, BorderLayout.NORTH);
+		pathsPanel.setLayout(new GridLayout(4, 1, 0, -5));
+
+		/****** Painel Ham ********/
+		JPanel hamPanel = new JPanel();
+		pathsPanel.add(hamPanel);
+
+		JLabel hamLabel = new JLabel("Ham:");
+		hamPanel.add(hamLabel);
+
+		hamTextField = new JTextField();
+		hamPanel.add(hamTextField);
+		hamTextField.setColumns(50);
+		/****************************************/
+
+		/****** Painel Spam ***********/
+		JPanel spamPanel = new JPanel();
+		pathsPanel.add(spamPanel);
+
+		JLabel spamLabel = new JLabel("Spam:");
+		spamPanel.add(spamLabel);
+
+		spamTextField = new JTextField();
+		spamPanel.add(spamTextField);
+		spamTextField.setColumns(50);
+		/******************************************/
+
+		/********* Painel Rules *********/
+		JPanel rulesPanel = new JPanel();
+		pathsPanel.add(rulesPanel);
+		rulesPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		JLabel rulesLabel = new JLabel("Rules:");
+		rulesPanel.add(rulesLabel);
+
+		rulesTextField = new JTextField();
+		rulesPanel.add(rulesTextField);
+		rulesTextField.setColumns(50);
+
+		JButton rulesUploadButton = new JButton("Upload");
+
+		rulesPanel.add(rulesUploadButton);
+		/******************************************/
+
+		/****** Painel config checkbox ********/
+		JPanel panelConfigChcbx = new JPanel();
+		pathsPanel.add(panelConfigChcbx);
+
+		JLabel labelConfig = new JLabel("Config:");
+		panelConfigChcbx.add(labelConfig);
+
+		JCheckBox chckbxManual = new JCheckBox("Manual");
+		panelConfigChcbx.add(chckbxManual);
+
+		JCheckBox chckbxAutomatica = new JCheckBox("Autom\u00E1tica");
+		panelConfigChcbx.add(chckbxAutomatica);
+
+		/* Falsos Positivos */
+		JLabel labelFP = new JLabel("FP");
+		panelConfigChcbx.add(labelFP);
+
+		fpTextField = new JTextField();
+		panelConfigChcbx.add(fpTextField);
+		fpTextField.setColumns(10);
+		fpTextField.setEditable(false);
+		/**/
+		/* Falsos Negativos */
+		JLabel labelFN = new JLabel("FN");
+		panelConfigChcbx.add(labelFN);
+
+		fnTextField = new JTextField();
+		panelConfigChcbx.add(fnTextField);
+		fnTextField.setColumns(10);
+		fnTextField.setEditable(false);
+		/**/
+		/************************************/
+
+		/******** Painel Inferior Avaliar+Gerar+Obter Grafico *************/
+		JPanel panelSouth = new JPanel();
+		frame.getContentPane().add(panelSouth, BorderLayout.SOUTH);
+		panelSouth.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		JButton buttonAvalConfig = new JButton("Avaliar Configura\u00E7\u00E3o Manual");
+		buttonAvalConfig.setHorizontalAlignment(SwingConstants.LEFT);
+		buttonAvalConfig.setVerticalAlignment(SwingConstants.BOTTOM);
+		panelSouth.add(buttonAvalConfig);
+
+		JButton buttonGerarConfig = new JButton("Gerar Configura\u00E7\u00E3o Autom\u00E1tica");
+		buttonGerarConfig.setHorizontalAlignment(SwingConstants.RIGHT);
+		panelSouth.add(buttonGerarConfig);
+
+		JButton buttonObterGrafico = new JButton("Obter Gr\u00E1fico");
+		panelSouth.add(buttonObterGrafico);
+
+		JButton buttonGravar = new JButton("Gravar");
+		panelSouth.add(buttonGravar);
+		buttonGravar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				writeRules();
 			}
 		});
-	
+		/***********************************************/
+
+		/* Tabela para regras */
+		table = new JTable();
+
+		/* Ação para carregar no botão e apresentar regras na tabela */
+		rulesUploadButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (chckbxManual.isSelected()) {
+					editable = true;
+					uploadRules(rulesTextField.getText());
+				}
+			}
+		});
+		;
+
 		JScrollPane scrollbar = new JScrollPane(table);
 		frame.getContentPane().add(scrollbar, BorderLayout.CENTER);
-		
-		
+
 	}
-	
+
+	/*
+	 * após pressionar o botão de upload com path na text box as regras e respetivos
+	 * pesos são apresentados numa tabela
+	 */
+	private void uploadRules(String rulesPath) {
+		this.rulesList = Rule.readRulesFile(rulesPath);
+		DefaultTableModel model = new DefaultTableModel() {
+
+			/* Vetor nomes das colunas */
+			String[] colNames = { "Rule", "Weight" };
+
+			/* Nomes das Colunas */
+			@Override
+			public String getColumnName(int column) {
+				// TODO Auto-generated method stub
+				return colNames[column];
+			}
+
+			/* Numero de Linhas */
+			@Override
+			public int getRowCount() {
+				return rulesList.size();
+			}
+
+			/* Numero de Colunas */
+			@Override
+			public int getColumnCount() {
+				return 2;
+			}
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return editable;
+			}
+		};
+
+		for (int i = 0; i < rulesList.size(); i++) {
+			Rule rule = rulesList.get(i);
+			String name = rule.getName();
+			model.setValueAt(name, i, 0);
+
+			if (rule.hasWeight()) {
+				int weight = rule.getWeight();
+				model.setValueAt(weight, i, 1);
+			}
+
+		}
+		table.setModel(model);
+	}
+
+	/* faz update na lista de regras */
+	private void updateList() {
+		for (int i = 0; i < rulesList.size(); i++) {
+			Rule rule = rulesList.get(i);
+			TableModel model = table.getModel();
+			String name = (String) (model.getValueAt(i, 0));
+			rule.setName(name);
+			if (model.getValueAt(i, 1) != null) {
+				String strWeight = (String) (model.getValueAt(i, 1));
+				int weight = Integer.parseInt(strWeight.trim());
+				rule.setWeight(weight);
+			}
+
+		}
+	}
+
+	/*Escreve as regras e respetivos pesos*/
+	private void writeRules() {
+		updateList();
+		try {
+			Writer writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(
+							"C:\\Users\\afons\\git\\ES1-2017-IC1-64\\ES1-2017-IC1-64\\files\\rules_v1.cf")));
+			String rules = Rule.rulesListToString(rulesList);
+			writer.write(rules);
+			writer.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("DONE");
+	}
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -191,5 +288,5 @@ public class Gui {
 			}
 		});
 	}
-	
+
 }
